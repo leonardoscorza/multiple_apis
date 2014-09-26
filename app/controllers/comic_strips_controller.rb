7b5567820ecf661d9d43eaac8cd1aaa8d0b58463
Leonardo Scorza <leonardoscorzas88@gmail.com>
2014-09-25 21:42:18 -0300

    asa

--- app/controllers/comic_strips_controller.rb
+++ app/controllers/comic_strips_controller.rb
@@ -5,6 +5,9 @@ class ComicStripsController < ApplicationController
 	def ramdom
 		doc = Nokogiri::HTML(open('http://c.xkcd.com/random/comic/'))
 		doc.encoding = 'utf-8'
-		render json: {ramdomUrl: doc.xpath("//img")[2].attributes['src'].value }
+		send_file( open(doc.xpath("//img")[2].attributes['src'].value),
+      	:disposition => 'inline',
+      	:type => 'image/jpeg',
+      	:x_sendfile => true )
 	end
 end
