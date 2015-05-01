817e79c8e4c8268b96b91c366c439087fad1b26e
Leonardo Scorza <leonardoscorzas88@gmail.com>
2015-05-01 17:59:55 -0300

    refresh comic html parse

--- app/controllers/comic_strips_controller.rb
+++ app/controllers/comic_strips_controller.rb
@@ -1,11 +1,11 @@
-require 'open-uri'
-
 class ComicStripsController < ApplicationController
+	require 'nokogiri'
+	require 'open-uri'
 
 	def ramdom
 		doc = Nokogiri::HTML(open('http://c.xkcd.com/random/comic/'))
 		doc.encoding = 'utf-8'
-		send_file( open(doc.xpath("//img")[2].attributes['src'].value),
+		send_file( open(doc.xpath("//img")[1].attributes['src'].value.sub! '//', 'http://' ),
       	:disposition => 'inline',
       	:type => 'image/jpeg',
       	:x_sendfile => true )
